8f774512d3a52687faf35a95c5dbb5b8a5d6e050
Christine Coulter <christinetcoulter@gmail.com>
2013-08-29 10:53:39 -0400

    Got first two.

--- spec/quizSpec.js
+++ spec/quizSpec.js
@@ -16,4 +16,25 @@ describe('a quiz', function() {
     expect(QUIZ_TOPICS).toContain('javascript');
   });
 
+  it('should find the largest number and second largest number, and then subtract the difference', function() {
+    expect(bigDiff([3, 6, 9, 12])).toEqual(3);
+    expect(bigDiff([9, 20, 11, 12])).toEqual(8);
+  });
+
+  it('should return the average of the 2 or 3 numbers in the middle of the array', function() {
+    expect(averageMedian([2, 6, 9, 11])).toEqual(7.5);
+    expect(averageMedian([9, 8, 2, 4, 7])).toEqual(4.67);
+  });
+
+  it('should return true if you can split the array into parts where the sum of one portion equals the sum of the other portion', function() {
+    expect(canBalance([1, 2, 1, 1, 1])).toEqual(true);
+    expect(canBalance([2, 1, 1, 2, 1])).toEqual(false);
+    expect(canBalance([3, 4, 1, 2, 3, 1])).toEqual(true);
+  });
+
+  it('should return the number of times 2 or more of the same number appear sequentially', function() {
+    expect(countClumps([2, 1, 1, 1, 1, 3, 3, 3, 1])).toEqual(2);
+    expect(countClumps([9, 3, 3, 4, 4, 4, 6, 7, 7, 7])).toEqual(3);
+  });
+
 });
--- src/quiz.js
+++ src/quiz.js
@@ -13,12 +13,48 @@ var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
 //  [3, 6, 9, 12] => 3
 //  [9, 20, 11, 12] => 8
 
+var bigDiff = function(array) {
+  var firstNum,
+      secondNum,
+      i,
+      j,
+      length = array.length;
+
+  Array.max = function(array){
+    return Math.max.apply(Math, array);
+  };
+
+  firstNum = Array.max(array);
+
+  array.splice(array.indexOf(firstNum), 1);
+
+  secondNum = Array.max(array);
+
+  return Math.abs(firstNum - secondNum);
+
+};
+
 // return the average of the 2 or 3 numbers in the middle of the array
 // (2 if the array has an even number of elements)
 // (3 if the array has an odd number of elements).
 // averageMedian(array)
 //  [2, 6, 9, 11] => 7.5
 //  [9, 8, 2, 4, 7] => 4.67
+
+var averageMedian = function(array) {
+  var length = array.length,
+      index = (length / 2),
+      median;
+
+  if ((length % 2) === 0) {
+    median = ((array[index] + array [index - 1]) / 2);
+    return median;
+  } else {
+    index = parseInt(index);
+    median = ((array[index - 1] + array[index] + array[index + 1]) / 3);
+    return Math.round(median * 100) / 100;
+  }
+};
 
 // return true if you can split the array into parts
 // where the sum of one portion equals the sum of the other portion.
@@ -27,7 +63,31 @@ var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
 //  [2, 1, 1, 2, 1] => false
 //  [3, 4, 1, 2, 3, 1] => true
 
+var canBalance = function(array) {
+  var giantArray = [],
+      i,
+      length = array.length;
+  for (i = 0; i < length; i++) {
+      var subArray = array.splice(0, i);
+      giantArray.push(subArray);
+  }
+  console.log(giantArray);
+};
+
 // Returns the number of times 2 or more of the same number appear sequentially.
 // countClumps(array)
 //  [2, 1, 1, 1, 1, 3, 3, 3, 1] => 2
 //  [9, 3, 3, 4, 4, 4, 6, 7, 7, 7] => 3
+
+var countClumps = function(array) {
+  var counter = 0,
+      i,
+      length = array.length;
+
+  for (i = 0; i < length; i++) {
+    if (array[i] === array[i + 1]) {
+      counter += 1;
+    }
+  }
+  return counter;
+};
